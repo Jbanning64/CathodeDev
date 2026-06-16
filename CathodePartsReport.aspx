c76be6cea57298d0277f2d3d7b00f5bcb156354c
jbanning64 <jeff.banning@atimaterials.com>
2026-06-16 11:55:35 -0700

    Adding tungsten report

---
+++ CathodePartsReport.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Cathode Parts Report" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CathodePartsReport.aspx.cs" Inherits="CathodeWeb.CathodePartsReport" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Cathode Parts Summary</h2>
+
+    <div style="margin-bottom:20px;">
+        <asp:Label runat="server" Text="Start Date:" AssociatedControlID="txtStart"></asp:Label>
+        <asp:TextBox ID="txtStart" runat="server" TextMode="Date"></asp:TextBox>
+
+        &nbsp;&nbsp;
+
+        <asp:Label runat="server" Text="End Date:" AssociatedControlID="txtEnd"></asp:Label>
+        <asp:TextBox ID="txtEnd" runat="server" TextMode="Date"></asp:TextBox>
+
+        &nbsp;&nbsp;
+
+        <asp:Button ID="btnRun" runat="server" Text="Run Report" OnClick="btnRun_Click" />
+    </div>
+
+    <asp:GridView ID="gvParts" runat="server" AutoGenerateColumns="false" CssClass="table">
+        <Columns>
+            <asp:BoundField DataField="CathodeParts" HeaderText="Cathode Parts" />
+            <asp:BoundField DataField="PartTotals" HeaderText="Total" />
+        </Columns>
+    </asp:GridView>
+
+</asp:Content>
---
+++ CathodePartsReport.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CathodeWeb
+{
+    public partial class CathodePartsReport : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRun_Click(object sender, EventArgs e)
+        {
+            DateTime start = DateTime.Parse(txtStart.Text);
+            DateTime end = DateTime.Parse(txtEnd.Text);
+
+            string sql = @"
+                SELECT 
+                    CASE
+                        WHEN p.description = 'Rod'
+                            THEN '4 Rod Set'
+                        ELSE
+                            p.description
+                    END AS CathodeParts,
+                    CASE
+                        WHEN p.description = 'Rod'
+                            THEN SUM(ph.partcount) / 4
+                        ELSE
+                            SUM(ph.partcount)
+                    END AS PartTotals
+                FROM parthistory ph
+                INNER JOIN parts p
+                    ON p.id = ph.parts_id
+                WHERE ph.created_at >= @start
+                AND ph.created_at < @end
+                GROUP BY p.description
+                ORDER BY p.description;
+            ";
+
+            using (SqlConnection conn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["CathodeConnString"].ConnectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@start", start);
+                cmd.Parameters.AddWithValue("@end", end);
+
+                conn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(rdr);
+
+                gvParts.DataSource = dt;
+                gvParts.DataBind();
+            }
+        }
+    }
+}
